7a94a9d6dfe3caedb7ba7fcd9d08680ce9a5b5d4
JOSE HERIBERTO <heriberto.perez@crowdint.com>
2015-07-14 16:17:54 -0500

    [#98580492] Adds missing class type reference for oportunity and estimate(quote)

--- lib/netsuite/records/opportunity.rb
+++ lib/netsuite/records/opportunity.rb
@@ -36,7 +36,7 @@ module NetSuite
       #  :consol_overdue_balance, :unbilled_orders, :consol_unbilled_orders
 
       record_refs :access_role, :custom_form, :currency, :entity_status, :partner, :category, :lead_source,
-        :price_level,:sales_rep, :subsidiary, :terms, :parent, :entity
+        :price_level,:sales_rep, :subsidiary, :terms, :parent, :entity, :klass
 
       attr_reader   :internal_id
       attr_accessor :external_id
